83085fdb60d1c41b31b9e96866e4e9c58a615278
Pierre EVEN <evenpierre@orange.fr>
2024-12-27 13:16:06 +0100

    added cmaa passes, still missing descriptors update

--- tests/test_editor/private/cmaa.cpp
+++ tests/test_editor/private/cmaa.cpp
@@ -4,7 +4,9 @@
 #include "object_ptr.hpp"
 #include "assets/material_asset.hpp"
 #include "gfx/renderer/definition/renderer.hpp"
+#include "gfx/vulkan/command_buffer.hpp"
 #include "gfx/vulkan/compute_pipeline.hpp"
+#include "gfx/vulkan/descriptor_sets.hpp"
 #include "gfx/vulkan/shader_module.hpp"
 #include "shader_compiler/shader_compiler.hpp"
 
@@ -18,6 +20,7 @@ public:
 
     void init(const Eng::Gfx::RenderPassInstanceBase&) override
     {
+        auto device                   = Eng::Engine::get().get_device();
         auto session                  = ShaderCompiler::Compiler::get().create_session("cmaa2");
         auto cr_cmaa2_edges_color_2x2 = session->compile("cmaa2_edges_color_2x2", session->get_default_permutations_description());
         if (!cr_cmaa2_edges_color_2x2.errors.empty())
@@ -26,12 +29,9 @@ public:
                 LOG_ERROR("Failed to compile cmaa2_edges_color_2x2 shader : {}:{} : {}", err.line + 1, err.column, err.message);
             return;
         }
-        LOG_INFO("Compiled cmaa2_edges_color_2x2");
-        std::shared_ptr<Eng::Gfx::ShaderModule> sm_cmaa2_edges_color_2x2 = Eng::Gfx::ShaderModule::create(Eng::Engine::get().get_device(), cr_cmaa2_edges_color_2x2.stages.find(Eng::Gfx::EShaderStage::Compute)->second);
-        LOG_INFO("Created cmaa2_edges_color_2x2 module");
-
-        std::shared_ptr<Eng::Gfx::ComputePipeline> pipeline_cmaa2_edges_color_2x2 = Eng::Gfx::ComputePipeline::create("cmaa2_edges_color_2x2", Eng::Engine::get().get_device(), sm_cmaa2_edges_color_2x2);
-        LOG_INFO("Created cmaa2_edges_color_2x2 pipeline");
+        std::shared_ptr<Eng::Gfx::ShaderModule> sm_cmaa2_edges_color_2x2 = Eng::Gfx::ShaderModule::create(device, cr_cmaa2_edges_color_2x2.stages.find(Eng::Gfx::EShaderStage::Compute)->second);
+        pipeline_cmaa2_edges_color_2x2                                   = Eng::Gfx::ComputePipeline::create("cmaa2_edges_color_2x2", device, sm_cmaa2_edges_color_2x2);
+        descriptors_cmaa2_edges_color_2x2                                = Eng::Gfx::DescriptorSet::create("cmaa2_edges_color_2x2", device, pipeline_cmaa2_edges_color_2x2->get_layout());
 
         auto cr_cmaa2_compute_dispatch_args = session->compile("cmaa2_compute_dispatch_args", session->get_default_permutations_description());
         if (!cr_cmaa2_compute_dispatch_args.errors.empty())
@@ -40,12 +40,9 @@ public:
                 LOG_ERROR("Failed to compile cmaa2_compute_dispatch_args shader : {}:{} : {}", err.line + 1, err.column, err.message);
             return;
         }
-        LOG_INFO("Compiled cmaa2_compute_dispatch_args");
-        std::shared_ptr<Eng::Gfx::ShaderModule> sm_cmaa2_compute_dispatch_args =
-            Eng::Gfx::ShaderModule::create(Eng::Engine::get().get_device(), cr_cmaa2_compute_dispatch_args.stages.find(Eng::Gfx::EShaderStage::Compute)->second);
-        LOG_INFO("Created cmaa2_compute_dispatch_args module");
-        std::shared_ptr<Eng::Gfx::ComputePipeline> pipeline_cmaa2_compute_dispatch_args = Eng::Gfx::ComputePipeline::create("cmaa2_compute_dispatch_args", Eng::Engine::get().get_device(), sm_cmaa2_compute_dispatch_args);
-        LOG_INFO("Created cmaa2_compute_dispatch_args pipeline");
+        std::shared_ptr<Eng::Gfx::ShaderModule> sm_cmaa2_compute_dispatch_args = Eng::Gfx::ShaderModule::create(device, cr_cmaa2_compute_dispatch_args.stages.find(Eng::Gfx::EShaderStage::Compute)->second);
+        pipeline_cmaa2_compute_dispatch_args                                   = Eng::Gfx::ComputePipeline::create("cmaa2_compute_dispatch_args", device, sm_cmaa2_compute_dispatch_args);
+        descriptors_cmaa2_compute_dispatch_args                                = Eng::Gfx::DescriptorSet::create("cmaa2_compute_dispatch_args", device, pipeline_cmaa2_compute_dispatch_args->get_layout());
 
         auto cr_cmaa2_process_candidates = session->compile("cmaa2_process_candidates", session->get_default_permutations_description());
         if (!cr_cmaa2_process_candidates.errors.empty())
@@ -54,12 +51,9 @@ public:
                 LOG_ERROR("Failed to compile cmaa2_process_candidates shader : {}:{} : {}", err.line + 1, err.column, err.message);
             return;
         }
-        LOG_INFO("Compiled cmaa2_process_candidates");
-        std::shared_ptr<Eng::Gfx::ShaderModule> sm_cmaa2_process_candidates =
-            Eng::Gfx::ShaderModule::create(Eng::Engine::get().get_device(), cr_cmaa2_process_candidates.stages.find(Eng::Gfx::EShaderStage::Compute)->second);
-        LOG_INFO("Created cmaa2_process_candidates module");
-        std::shared_ptr<Eng::Gfx::ComputePipeline> pipeline_cmaa2_process_candidates = Eng::Gfx::ComputePipeline::create("cmaa2_process_candidates", Eng::Engine::get().get_device(), sm_cmaa2_process_candidates);
-        LOG_INFO("Created cmaa2_process_candidates pipeline");
+        std::shared_ptr<Eng::Gfx::ShaderModule> sm_cmaa2_process_candidates = Eng::Gfx::ShaderModule::create(device, cr_cmaa2_process_candidates.stages.find(Eng::Gfx::EShaderStage::Compute)->second);
+        pipeline_cmaa2_process_candidates                                   = Eng::Gfx::ComputePipeline::create("cmaa2_process_candidates", device, sm_cmaa2_process_candidates);
+        descriptors_cmaa2_process_candidates                                = Eng::Gfx::DescriptorSet::create("cmaa2_process_candidates", device, pipeline_cmaa2_process_candidates->get_layout());
 
         auto cr_cmaa2_deferred_color_apply2x2 = session->compile("cmaa2_deferred_color_apply2x2", session->get_default_permutations_description());
         if (!cr_cmaa2_deferred_color_apply2x2.errors.empty())
@@ -68,13 +62,9 @@ public:
                 LOG_ERROR("Failed to compile cmaa2_deferred_color_apply2x2 shader : {}:{} : {}", err.line + 1, err.column, err.message);
             return;
         }
-        LOG_INFO("Compiled cmaa2_deferred_color_apply2x2");
-        std::shared_ptr<Eng::Gfx::ShaderModule> sm_cmaa2_deferred_color_apply2x2 =
-            Eng::Gfx::ShaderModule::create(Eng::Engine::get().get_device(), cr_cmaa2_deferred_color_apply2x2.stages.find(Eng::Gfx::EShaderStage::Compute)->second);
-        LOG_INFO("Created cmaa2_deferred_color_apply2x2 module");
-        std::shared_ptr<Eng::Gfx::ComputePipeline> pipeline_cmaa2_deferred_color_apply2x2 =
-            Eng::Gfx::ComputePipeline::create("cmaa2_deferred_color_apply2x2", Eng::Engine::get().get_device(), sm_cmaa2_deferred_color_apply2x2);
-        LOG_INFO("Created cmaa2_deferred_color_apply2x2 pipeline");
+        std::shared_ptr<Eng::Gfx::ShaderModule> sm_cmaa2_deferred_color_apply2x2 = Eng::Gfx::ShaderModule::create(device, cr_cmaa2_deferred_color_apply2x2.stages.find(Eng::Gfx::EShaderStage::Compute)->second);
+        pipeline_cmaa2_deferred_color_apply2x2                                   = Eng::Gfx::ComputePipeline::create("cmaa2_deferred_color_apply2x2", device, sm_cmaa2_deferred_color_apply2x2);
+        descriptors_cmaa2_deferred_color_apply2x2                                = Eng::Gfx::DescriptorSet::create("cmaa2_deferred_color_apply2x2", device, pipeline_cmaa2_deferred_color_apply2x2->get_layout());
 
         auto cr_cmaa2_debug_draw_edges = session->compile("cmaa2_debug_draw_edges", session->get_default_permutations_description());
         if (!cr_cmaa2_debug_draw_edges.errors.empty())
@@ -83,11 +73,9 @@ public:
                 LOG_ERROR("Failed to compile cmaa2_debug_draw_edges shader : {}:{} : {}", err.line + 1, err.column, err.message);
             return;
         }
-        LOG_INFO("Compiled cmaa2_debug_draw_edges");
-        std::shared_ptr<Eng::Gfx::ShaderModule> sm_cmaa2_debug_draw_edges = Eng::Gfx::ShaderModule::create(Eng::Engine::get().get_device(), cr_cmaa2_debug_draw_edges.stages.find(Eng::Gfx::EShaderStage::Compute)->second);
-        LOG_INFO("Created cmaa2_debug_draw_edges module");
-        std::shared_ptr<Eng::Gfx::ComputePipeline> pipeline_cmaa2_debug_draw_edges = Eng::Gfx::ComputePipeline::create("cmaa2_debug_draw_edges", Eng::Engine::get().get_device(), sm_cmaa2_debug_draw_edges);
-        LOG_INFO("Created cmaa2_debug_draw_edges pipeline");
+        std::shared_ptr<Eng::Gfx::ShaderModule> sm_cmaa2_debug_draw_edges = Eng::Gfx::ShaderModule::create(device, cr_cmaa2_debug_draw_edges.stages.find(Eng::Gfx::EShaderStage::Compute)->second);
+        pipeline_cmaa2_debug_draw_edges                                   = Eng::Gfx::ComputePipeline::create("cmaa2_debug_draw_edges", device, sm_cmaa2_debug_draw_edges);
+        descriptors_cmaa2_debug_draw_edges                                = Eng::Gfx::DescriptorSet::create("cmaa2_debug_draw_edges", device, pipeline_cmaa2_debug_draw_edges->get_layout());
 
     }
 
@@ -96,23 +84,55 @@ public:
 
     }
 
-    void draw(const Eng::Gfx::RenderPassInstanceBase&, Eng::Gfx::CommandBuffer&, size_t) override
+    void draw(const Eng::Gfx::RenderPassInstanceBase&, Eng::Gfx::CommandBuffer& cmd, size_t) override
     {
+        cmd.bind_compute_pipeline(*pipeline_cmaa2_edges_color_2x2);
+        cmd.bind_descriptors(*descriptors_cmaa2_edges_color_2x2, *pipeline_cmaa2_edges_color_2x2);
+        cmd.dispatch_compute();
 
+        cmd.bind_compute_pipeline(*pipeline_cmaa2_compute_dispatch_args);
+        cmd.bind_descriptors(*descriptors_cmaa2_compute_dispatch_args, *pipeline_cmaa2_compute_dispatch_args);
+        cmd.dispatch_compute();
+
+        cmd.bind_compute_pipeline(*pipeline_cmaa2_process_candidates);
+        cmd.bind_descriptors(*descriptors_cmaa2_process_candidates, *pipeline_cmaa2_process_candidates);
+        cmd.dispatch_compute();
+
+        cmd.bind_compute_pipeline(*pipeline_cmaa2_deferred_color_apply2x2);
+        cmd.bind_descriptors(*descriptors_cmaa2_deferred_color_apply2x2, *pipeline_cmaa2_deferred_color_apply2x2);
+        cmd.dispatch_compute();
+
+        cmd.bind_compute_pipeline(*pipeline_cmaa2_debug_draw_edges);
+        cmd.bind_descriptors(*descriptors_cmaa2_debug_draw_edges, *pipeline_cmaa2_debug_draw_edges);
+        cmd.dispatch_compute();
     }
 
     void pre_submit(const Eng::Gfx::RenderPassInstanceBase&) override
     {
 
     }
-};
 
+private:
+    std::shared_ptr<Eng::Gfx::ComputePipeline> pipeline_cmaa2_edges_color_2x2;
+    std::shared_ptr<Eng::Gfx::DescriptorSet>   descriptors_cmaa2_edges_color_2x2;
+
+    std::shared_ptr<Eng::Gfx::ComputePipeline> pipeline_cmaa2_compute_dispatch_args;
+    std::shared_ptr<Eng::Gfx::DescriptorSet>   descriptors_cmaa2_compute_dispatch_args;
+
+    std::shared_ptr<Eng::Gfx::ComputePipeline> pipeline_cmaa2_process_candidates;
+    std::shared_ptr<Eng::Gfx::DescriptorSet>   descriptors_cmaa2_process_candidates;
+
+    std::shared_ptr<Eng::Gfx::ComputePipeline> pipeline_cmaa2_deferred_color_apply2x2;
+    std::shared_ptr<Eng::Gfx::DescriptorSet>   descriptors_cmaa2_deferred_color_apply2x2;
+
+    std::shared_ptr<Eng::Gfx::ComputePipeline> pipeline_cmaa2_debug_draw_edges;
+    std::shared_ptr<Eng::Gfx::DescriptorSet>   descriptors_cmaa2_debug_draw_edges;
+};
 
 void Cmaa2::append_to_renderer(Eng::Gfx::Renderer& renderer)
 {
     renderer["cmaa2"]
         .require("gbuffer_resolve")
-        //.require("gbuffers")
         .render_pass<CmaaRenderPass>()
         .compute_pass(true)
         [Eng::Gfx::Attachment::slot("target").format(Eng::Gfx::ColorFormat::R8G8B8A8_UNORM)];
