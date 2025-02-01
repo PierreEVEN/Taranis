725f01f8408b47ce50f093175a12f0e2e886bbfa
Pierre EVEN <evenpierre@orange.fr>
2025-02-01 11:08:12 +0100

    first 'patatoide'

--- tests/test_editor/private/procedural_planet/planet_component.cpp
+++ tests/test_editor/private/procedural_planet/planet_component.cpp
@@ -10,92 +10,143 @@
 
 #include "third_party/fastnoise/FastNoise.h"
 
-PlanetComponent::PlanetComponent()
+#include <numbers>
+
+struct PlanetSectionVertex
 {
-    fast_noise = std::make_shared<FastNoise>();
+    glm::vec3 pos;
+    glm::vec2 uv;
+    glm::vec3 norm;
+};
 
-    struct PlanetSectionVertex
-    {
-        glm::vec3 pos;
-        glm::vec2 uv;
-        glm::vec3 norm;
-    };
+static float make_noise(const glm::vec3& source, const FastNoise& noise)
+{
+    return noise.GetSimplex(source.x * 1000.f, source.y * 1000.f, source.z * 1000.f) * 0.4f
+    + noise.GetSimplex(source.x * 200.f, source.y * 200.f, source.z * 200.f) * 0.7f
+    + noise.GetSimplex(source.x * 100.f, source.y * 100.f, source.z * 100.f) * 4.2f;
+}
 
+PlanetSection::PlanetSection(PlanetComponent& in_root, int in_level, const glm::mat4& in_transform) : transform(in_transform), root(in_root), level(in_level)
+{
+    uint32_t                         res = 200;
     std::vector<PlanetSectionVertex> vertices;
     std::vector<uint32_t>            indices;
-
-    size_t width = 1000;
-
-    constexpr float mesh_scale = 50.f;
-
-    // Generate vertices
-    for (size_t x = 0; x < width; ++x)
+    generate_square_section(vertices, indices, res);
+    auto rot = glm::mat3(in_transform);
+    for (auto& vertex : vertices)
     {
-        for (size_t y = 0; y < width; ++y)
-        {
-            float p_x  = (static_cast<float>(x) / static_cast<float>(width) * 2.f - 1.f);
-            float p_y  = (static_cast<float>(y) / static_cast<float>(width) * 2.f - 1.f);
-            float p_z  = 1;
-            float p_xc = p_x * (std::sqrt(1 - p_y * p_y / 2 - p_z * p_z / 2 + p_y * p_y * p_z * p_z / 3));
-            float p_yc = p_y * (std::sqrt(1 - p_x * p_x / 2 - p_z * p_z / 2 + p_x * p_x * p_z * p_z / 3));
-            float p_zc = p_z * (std::sqrt(1 - p_y * p_y / 2 - p_x * p_x / 2 + p_y * p_y * p_x * p_x / 3));
+        vertex.pos = rot * vertex.pos;
 
-            float z = fast_noise->GetSimplex(p_xc * 1000.f, p_yc * 1000.f, p_zc * 1000.f) * 0.9f;
-            vertices.emplace_back(PlanetSectionVertex{glm::vec3{p_xc, p_yc, p_zc} * (mesh_scale + z), {x, y}, {0, 0, 1}});
+        const float z = make_noise(vertex.pos, *root.fast_noise);
+        vertex.pos    = sphere_mapping(vertex.pos) * (100 + z);
+    }
+    generate_normals(vertices, res);
+
+    auto device = Eng::Engine::get().get_device().lock();
+
+    Eng::Gfx::BufferData index_buffer(indices);
+    section_mesh = Eng::Gfx::Mesh::create("PlanetSection", device, Eng::Gfx::EBufferType::IMMUTABLE, Eng::Gfx::BufferData(vertices), &index_buffer);
+}
+
+void PlanetSection::generate_square_section(std::vector<PlanetSectionVertex>& vertices, std::vector<uint32_t>& indices, uint32_t res)
+{
+    // Generate vertices
+    for (size_t x = 0; x < res; ++x)
+    {
+        for (size_t y = 0; y < res; ++y)
+        {
+            float p_x = static_cast<float>(x) / static_cast<float>(res - 1) * 2.f - 1.f;
+            float p_y = static_cast<float>(y) / static_cast<float>(res - 1) * 2.f - 1.f;
+            float p_z = 1;
+            vertices.emplace_back(PlanetSectionVertex{glm::vec3{p_x, p_y, p_z}, {x, y}, {0, 0, 1}});
         }
     }
-    // Compute normals
-    for (size_t x = 1; x < width - 1; ++x)
+    // Generate index buffer
+    for (size_t x = 0; x < res - 1; ++x)
     {
-        for (size_t y = 1; y < width - 1; ++y)
+        for (size_t y = 0; y < res - 1; ++y)
         {
-            const glm::vec3& p = vertices[x + y * width].pos;
+            indices.emplace_back(x + y * res);
+            indices.emplace_back(x + 1 + (y + 1) * res);
+            indices.emplace_back(x + 1 + y * res);
 
-            glm::vec3 pa = vertices[x + 1 + y * width].pos - p;
-            glm::vec3 pb = vertices[x + (y + 1) * width].pos - p;
-            glm::vec3 pc = vertices[x - 1 + y * width].pos - p;
-            glm::vec3 pd = vertices[x + (y - 1) * width].pos - p;
+            indices.emplace_back(x + y * res);
+            indices.emplace_back(x + (y + 1) * res);
+            indices.emplace_back(x + 1 + (y + 1) * res);
+        }
+    }
+}
+
+void PlanetSection::draw(Eng::Gfx::CommandBuffer& command_buffer, const Eng::SceneView& view)
+{
+    if (!children.empty())
+        for (auto& child : children)
+            child.draw(command_buffer, view);
+    else
+    {
+        command_buffer.draw_mesh(*section_mesh);
+    }
+}
+
+void PlanetSection::generate_normals(std::vector<PlanetSectionVertex>& vertices, uint32_t res)
+{
+    // Compute normals
+    for (size_t x = 1; x < res - 1; ++x)
+    {
+        for (size_t y = 1; y < res - 1; ++y)
+        {
+            const glm::vec3& p = vertices[x + y * res].pos;
+
+            glm::vec3 pa = vertices[x + 1 + y * res].pos - p;
+            glm::vec3 pb = vertices[x + (y + 1) * res].pos - p;
+            glm::vec3 pc = vertices[x - 1 + y * res].pos - p;
+            glm::vec3 pd = vertices[x + (y - 1) * res].pos - p;
 
             glm::vec3 n1 = cross(normalize(pb), normalize(pa));
             glm::vec3 n2 = cross(normalize(pd), normalize(pc));
 
-            vertices[x + y * width].norm = normalize((n1 + n2) / 2.f);
+            vertices[x + y * res].norm = normalize((n1 + n2) / 2.f);
         }
     }
+}
 
-    // Generate index buffer
-    for (size_t x = 0; x < width - 1; ++x)
-    {
-        for (size_t y = 0; y < width - 1; ++y)
-        {
-            indices.emplace_back(x + y * width);
-            indices.emplace_back(x + 1 + (y + 1) * width);
-            indices.emplace_back(x + 1 + y * width);
+glm::vec3 PlanetSection::sphere_mapping(const glm::vec3& source)
+{
+    return {source.x * std::sqrt(1 - source.y * source.y / 2 - source.z * source.z / 2 + source.y * source.y * source.z * source.z / 3),
+            source.y * std::sqrt(1 - source.x * source.x / 2 - source.z * source.z / 2 + source.x * source.x * source.z * source.z / 3),
+            source.z * std::sqrt(1 - source.y * source.y / 2 - source.x * source.x / 2 + source.y * source.y * source.x * source.x / 3)};
+}
 
-            indices.emplace_back(x + y * width);
-            indices.emplace_back(x + (y + 1) * width);
-            indices.emplace_back(x + 1 + (y + 1) * width);
-        }
-    }
+PlanetComponent::PlanetComponent()
+{
+    fast_noise = std::make_shared<FastNoise>();
 
-    auto                 device = Eng::Engine::get().get_device().lock();
-    Eng::Gfx::BufferData index_buffer(indices);
-    mesh          = Eng::Gfx::Mesh::create("PlanetSection", device, Eng::Gfx::EBufferType::IMMUTABLE, Eng::Gfx::BufferData(vertices), &index_buffer);
+    auto device   = Eng::Engine::get().get_device().lock();
     auto base_mat = Eng::Engine::get().asset_registry().create<Eng::MaterialAsset>("PlanetMaterial");
     base_mat->set_shader_code("planet_mat", std::vector{
                                   StageInputOutputDescription{0, 0, Eng::Gfx::ColorFormat::R32G32B32_SFLOAT},
                                   StageInputOutputDescription{1, 12, Eng::Gfx::ColorFormat::R32G32_SFLOAT},
                                   StageInputOutputDescription{2, 20, Eng::Gfx::ColorFormat::R32G32B32_SFLOAT},
                               });
-    material = Eng::Engine::get().asset_registry().create<Eng::MaterialInstanceAsset>("PlanetMaterialInst", base_mat);
+    base_material = Eng::Engine::get().asset_registry().create<Eng::MaterialInstanceAsset>("PlanetMaterialInst", base_mat);
+
+    roots.emplace_back(std::make_shared<PlanetSection>(*this, 0, mat4_cast(glm::quat(glm::vec3(0.f, 0.f, 0.f)))));
+    roots.emplace_back(std::make_shared<PlanetSection>(*this, 0, mat4_cast(glm::quat(glm::vec3(std::numbers::pi, 0.f, 0.f)))));
+    roots.emplace_back(std::make_shared<PlanetSection>(*this, 0, mat4_cast(glm::quat(glm::vec3(0.f, std::numbers::pi * -0.5f, 0.f)))));
+    roots.emplace_back(std::make_shared<PlanetSection>(*this, 0, mat4_cast(glm::quat(glm::vec3(0.f, std::numbers::pi * 0.5f, 0.f)))));
+    roots.emplace_back(std::make_shared<PlanetSection>(*this, 0, mat4_cast(glm::quat(glm::vec3(std::numbers::pi * -0.5f, 0.f, 0.f)))));
+    roots.emplace_back(std::make_shared<PlanetSection>(*this, 0, mat4_cast(glm::quat(glm::vec3(std::numbers::pi * 0.5f, 0.f, 0.f)))));
+
 }
 
 void PlanetComponent::draw(Eng::Gfx::CommandBuffer& command_buffer, const Eng::SceneView& view)
 {
-    material->set_scene_data(command_buffer.render_pass(), view.get_view_buffer());
-    auto mat = material->get_base_resource(command_buffer.render_pass());
+    base_material->set_scene_data(command_buffer.render_pass(), view.get_view_buffer());
+    auto mat = base_material->get_base_resource(command_buffer.render_pass());
     command_buffer.bind_pipeline(mat);
     command_buffer.push_constant(Eng::Gfx::EShaderStage::Vertex, *mat, Eng::Gfx::BufferData(glm::identity<glm::mat4>()));
-    command_buffer.bind_descriptors(*material->get_descriptor_resource(command_buffer.render_pass()), *mat);
-    command_buffer.draw_mesh(*mesh);
+    command_buffer.bind_descriptors(*base_material->get_descriptor_resource(command_buffer.render_pass()), *mat);
+
+    for (auto& root : roots)
+        root->draw(command_buffer, view);
 }
--- tests/test_editor/private/procedural_planet/planet_component.hpp
+++ tests/test_editor/private/procedural_planet/planet_component.hpp
@@ -2,6 +2,7 @@
 #include "scene/components/primitive_component.hpp"
 #include "procedural_planet/planet_component.gen.hpp"
 
+struct PlanetSectionVertex;
 class FastNoise;
 
 namespace Eng
@@ -15,19 +16,41 @@ class MaterialInstanceAsset;
 class SceneView;
 }
 
+class PlanetSection
+{
+public:
+    PlanetSection(PlanetComponent& in_root, int in_level, const glm::mat4& in_transform);
+
+    void draw(Eng::Gfx::CommandBuffer& command_buffer, const Eng::SceneView& view);
+
+    static void generate_square_section(std::vector<PlanetSectionVertex>& vertices, std::vector<uint32_t>& indices, uint32_t res);
+    static void generate_normals(std::vector<PlanetSectionVertex>& vertices, uint32_t res);
+    static glm::vec3 sphere_mapping(const glm::vec3& source);
+
+private:
+    std::vector<PlanetSection> children;
+
+    glm::mat4                         transform;
+    std::shared_ptr<Eng::Gfx::Buffer> terrain_data;
+    PlanetComponent&                  root;
+    int                               level = 0;
+
+    std::shared_ptr<Eng::Gfx::Mesh> section_mesh;
+};
+
 class PlanetComponent : public Eng::PrimitiveComponent
 {
 public:
-            
     REFLECT_BODY();
 
     PlanetComponent();
 
-    virtual void draw(Eng::Gfx::CommandBuffer& command_buffer, const Eng::SceneView& view) override;
+    void draw(Eng::Gfx::CommandBuffer& command_buffer, const Eng::SceneView& view) override;
 
-    std::shared_ptr<Eng::Gfx::Mesh>        mesh;
-    TObjectRef<Eng::MaterialInstanceAsset> material;
+    TObjectRef<Eng::MaterialInstanceAsset> base_material;
 
-private:
     std::shared_ptr<FastNoise> fast_noise;
+
+  private:
+    std::vector<std::shared_ptr<PlanetSection>> roots;
 };
