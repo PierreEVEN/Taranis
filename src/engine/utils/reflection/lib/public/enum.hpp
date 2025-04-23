e66e27f4ff862b5abd46ac733433e2586ed100ac
Pierre EVEN <spam@evenpierre.fr>
2025-04-23 17:58:17 +0200

    enum is now a common type

--- src/engine/utils/reflection/lib/public/enum.hpp
+++ src/engine/utils/reflection/lib/public/enum.hpp
@@ -1,16 +1,19 @@
 #pragma once
+#include "type.hpp"
 #include "type_id.hpp"
 
 #include <ankerl/unordered_dense.h>
 
 namespace Reflection
 {
-class Enum
+class Enum : public Type
 {
 public:
     template <typename T> static Enum* register_enum()
     {
-        return register_enum_internal(TypeId::create<T>(), sizeof(T));
+        Enum* new_enum = register_enum_internal(TypeId::create<T>(), sizeof(T));
+        register_type_internal(new_enum);
+        return new_enum;
     }
 
     void register_field(const std::string& field);
@@ -18,16 +21,6 @@ public:
     static const ankerl::unordered_dense::map<TypeId, Enum*>& get_enums()
     {
         return get_registered_enums_internal();
-    }
-
-    const char* name() const
-    {
-        return type.name();
-    }
-
-    uint32_t stride() const
-    {
-        return size;
     }
 
     const ankerl::unordered_dense::set<std::string>& fields() const
@@ -38,15 +31,12 @@ public:
 private:
     static Enum* register_enum_internal(const TypeId& enum_id, uint32_t in_size);
 
-    Enum(const TypeId& in_type, uint32_t in_size) : type(in_type), size(in_size)
+    Enum(const TypeId& in_type, uint32_t in_size) : Type(in_type, in_size)
     {
     }
 
     static ankerl::unordered_dense::map<TypeId, Enum*>& get_registered_enums_internal();
     static ankerl::unordered_dense::map<TypeId, Enum*>* registered_enums;
-    ankerl::unordered_dense::set<std::string>          enum_fields;
-
-    TypeId   type;
-    uint32_t size;
+    ankerl::unordered_dense::set<std::string>           enum_fields;
 };
 }
